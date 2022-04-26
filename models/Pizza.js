266b270d2fe53d4749a7bb4f7c17661d5893ea81
Jarod Florence <jarod.florence@outlook.com>
2022-04-26 11:25:13 -0500

    rooutes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,26 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-      type: String
+      type: String,
     },
     createdBy: {
-      type: String
+      type: String,
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
     },
     size: {
       type: String,
-      default: 'Large'
+      default: "Large",
     },
-    toppings: []
+    toppings: [],
     // toppings: Array    <-- would work the same as brackets
-  });
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
 
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
